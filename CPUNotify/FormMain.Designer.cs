fc514ae7e4a58e56bd81d4a26def10cb2a0b813c
ambiesoft <ambiesoft.trueff@gmail.com>
2020-04-21 20:36:45 +0900

    start

---
+++ CPUNotify/FormMain.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace CPUNotify
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timerMain = new System.Windows.Forms.Timer(this.components);
+            this.lblCPU = new System.Windows.Forms.Label();
+            this.txtCpuUsage = new System.Windows.Forms.TextBox();
+            this.txtApp = new System.Windows.Forms.TextBox();
+            this.txtArg = new System.Windows.Forms.TextBox();
+            this.lblApp = new System.Windows.Forms.Label();
+            this.lblArg = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timerMain
+            // 
+            this.timerMain.Tick += new System.EventHandler(this.timerMain_Tick);
+            // 
+            // lblCPU
+            // 
+            this.lblCPU.AutoSize = true;
+            this.lblCPU.Location = new System.Drawing.Point(12, 9);
+            this.lblCPU.Name = "lblCPU";
+            this.lblCPU.Size = new System.Drawing.Size(66, 13);
+            this.lblCPU.TabIndex = 0;
+            this.lblCPU.Text = "&CPU Usage:";
+            // 
+            // txtCpuUsage
+            // 
+            this.txtCpuUsage.Location = new System.Drawing.Point(15, 25);
+            this.txtCpuUsage.Name = "txtCpuUsage";
+            this.txtCpuUsage.ReadOnly = true;
+            this.txtCpuUsage.Size = new System.Drawing.Size(488, 20);
+            this.txtCpuUsage.TabIndex = 1;
+            // 
+            // txtApp
+            // 
+            this.txtApp.Location = new System.Drawing.Point(23, 185);
+            this.txtApp.Name = "txtApp";
+            this.txtApp.Size = new System.Drawing.Size(480, 20);
+            this.txtApp.TabIndex = 2;
+            // 
+            // txtArg
+            // 
+            this.txtArg.Location = new System.Drawing.Point(23, 229);
+            this.txtArg.Name = "txtArg";
+            this.txtArg.Size = new System.Drawing.Size(480, 20);
+            this.txtArg.TabIndex = 3;
+            // 
+            // lblApp
+            // 
+            this.lblApp.AutoSize = true;
+            this.lblApp.Location = new System.Drawing.Point(20, 169);
+            this.lblApp.Name = "lblApp";
+            this.lblApp.Size = new System.Drawing.Size(29, 13);
+            this.lblApp.TabIndex = 4;
+            this.lblApp.Text = "&App:";
+            // 
+            // lblArg
+            // 
+            this.lblArg.AutoSize = true;
+            this.lblArg.Location = new System.Drawing.Point(20, 213);
+            this.lblArg.Name = "lblArg";
+            this.lblArg.Size = new System.Drawing.Size(26, 13);
+            this.lblArg.TabIndex = 5;
+            this.lblArg.Text = "Ar&g:";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(557, 347);
+            this.Controls.Add(this.lblArg);
+            this.Controls.Add(this.lblApp);
+            this.Controls.Add(this.txtArg);
+            this.Controls.Add(this.txtApp);
+            this.Controls.Add(this.txtCpuUsage);
+            this.Controls.Add(this.lblCPU);
+            this.Name = "FormMain";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timerMain;
+        private System.Windows.Forms.Label lblCPU;
+        private System.Windows.Forms.TextBox txtCpuUsage;
+        private System.Windows.Forms.TextBox txtApp;
+        private System.Windows.Forms.TextBox txtArg;
+        private System.Windows.Forms.Label lblApp;
+        private System.Windows.Forms.Label lblArg;
+    }
+}
+
---
+++ CPUNotify/FormMain.cs
@@ -0,0 +1,132 @@
+﻿using NDesk.Options;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CPUNotify
+{
+    public partial class FormMain : Form
+    {
+        
+        float? _minCPUUsage;
+        float? _maxCPUUsage;
+        int? _checkDuration;
+
+        int _totalHits = 0;
+        int _timerInterval = 10 * 1000;
+        PerformanceCounter _cpuCounter = new PerformanceCounter();
+        public FormMain(string[] args)
+        {
+            InitializeComponent();
+
+            try
+            {
+                var p = new OptionSet();
+                p.Add("v|version", "Show Version", dummy =>
+                {
+                    MessageBox.Show("ver");
+                    Environment.Exit(0);
+                });
+                p.Add("max=", "Max cpu usage", max =>
+                {
+                    _maxCPUUsage = float.Parse(max);
+                });
+                p.Add("min=", "Min cpu usage", max =>
+                {
+                    _minCPUUsage = float.Parse(max);
+                });
+                p.Add("d=|duration=", "Duration of checking", duration =>
+                {
+                    _checkDuration = int.Parse(duration);
+                });
+                p.Add("h|H|help|?", "Show Help", dummy =>
+                {
+                    p.WriteOptionDescriptions(Console.Out);
+                });
+
+                List<string> mainArgs = p.Parse(args);
+                if (mainArgs.Count != 0)
+                {
+                    FatalExit("Uknown Option:" + string.Join(" ", mainArgs.ToArray()));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Environment.Exit(1);
+            }
+
+            // check options
+            if (_minCPUUsage == null && _maxCPUUsage == null)
+            {
+                FatalExit("Min and max are both null");
+            }
+            if (_minCPUUsage == null)
+                _minCPUUsage = 100;
+            if (_maxCPUUsage == null)
+                _maxCPUUsage = 0;
+
+            if (_checkDuration == null)
+                FatalExit("Duration is empty");
+
+            _cpuCounter.CategoryName = "Processor";
+            _cpuCounter.CounterName = "% Processor Time";
+            _cpuCounter.InstanceName = "_Total";
+            // will always start at 0
+            _cpuCounter.NextValue();
+
+            timerMain.Interval = _timerInterval;
+            timerMain.Enabled = true;
+
+          
+        }
+
+        void FatalExit(string message)
+        {
+            MessageBox.Show(message);
+            Environment.Exit(1);
+        }
+        // https://stackoverflow.com/a/6168408
+        public float getCPUCounter()
+        {
+            // now matches task manager reading
+            return _cpuCounter.NextValue();
+        }
+
+        private void timerMain_Tick(object sender, EventArgs e)
+        {
+            float cpuPercent = getCPUCounter();
+
+            txtCpuUsage.Text = string.Format("{0}Hits in {1}, current cpu '{2}%'",
+                _totalHits, _checkDuration, cpuPercent);
+
+            if (_minCPUUsage <= cpuPercent && cpuPercent <= _maxCPUUsage)
+            {
+                _totalHits++;
+                if (_totalHits == _checkDuration)
+                {
+                    try
+                    {
+                        Process.Start(txtApp.Text, txtArg.Text);
+                    }
+                    catch(Exception ex)
+                    {
+                        FatalExit(ex.Message);
+                    }
+
+                    Close();
+                }
+            }
+            else
+            {
+                _totalHits = 0;
+            }
+        }
+    }
+}
